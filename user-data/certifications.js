fcee47371a5fa0c23a9ce0e1fad0de094593f3d5
KAPPI1925 <kamaleshkanna.skk@gmail.com>
2026-02-08 19:12:31 +0530

    acadamic and certificates updated

--- user-data/certifications.js
+++ user-data/certifications.js
@@ -3,65 +3,65 @@
 const certificationsData = {
   webCourses: {
     ESRI: [
-      { title: "Getting Started with Spatial Analysis", year: "2025", certificatePath: "certifications/esri/spatial-analysis.jpg" },
-      { title: "GIS Basics", year: "2023", certificatePath: "certifications/esri/vegetation_chart.png" },
-      { title: "ArcGIS Online Basics", year: "2023", certificatePath: "certifications/esri/arcgis-online.jpg" },
-      { title: "ArcGIS Pro Basics", year: "2023", certificatePath: "certifications/esri/arcgis-pro.jpg" },
-      { title: "Getting Started with Data Management", year: "2023", certificatePath: "certifications/esri/data-management.jpg" },
-      { title: "Getting Started with GIS", year: "2023", certificatePath: "certifications/esri/getting-started-gis.jpg" }
+      { title: "Getting Started with Spatial Analysis", year: "2025", certificatePath: "certifications/Course and Softwares/ESRI/Web Course - ESRI - Getting Started with Spatial Analysis_page_1.jpg" },
+      { title: "GIS Basics", year: "2023", certificatePath: "certifications/Course and Softwares/ESRI/Web Course - ESRI - GIS Basics_page_1.jpg" },
+      { title: "ArcGIS Online Basics", year: "2023", certificatePath: "certifications/Course and Softwares/ESRI/Web Course - ESRI - ArcGIS Online Basics_page_1.jpg" },
+      { title: "ArcGIS Pro Basics", year: "2023", certificatePath: "certifications/Course and Softwares/ESRI/Web Course - ESRI - ArcGIS Pro Basics_page_1.jpg" },
+      { title: "Getting Started with Data Management", year: "2023", certificatePath: "certifications/Course and Softwares/ESRI/Web Course - ESRI - Getting Started with Data Management_page_1.jpg" },
+      { title: "Getting Started with GIS", year: "2023", certificatePath: "certifications/Course and Softwares/ESRI/Web Course - ESRI - Getting Started with GIS_page_1.jpg" }
     ],
     ISRO: [
-      { title: "Primer on Interferometric SAR Remote Sensing (IIRS-NASA)", year: "2025", certificatePath: "certifications/isro/sar-remote-sensing.jpg" },
-      { title: "Remote Sensing and Digital Image Analysis (IIRS)", year: "2025", certificatePath: "certifications/isro/image-analysis.jpg" },
-      { title: "Remote Sensing Data Analytics for Crop Production Forecasting (IIRS) [A+]", year: "2025", certificatePath: "certifications/isro/crop-forecasting.jpg" },
-      { title: "Geospatial technology for modelling of Urban Environment (IIRS) [A+]", year: "2025", certificatePath: "certifications/isro/urban-environment.jpg" },
-      { title: "Space Technology for Disaster Risk Reduction (IIRS) [A+]", year: "2025", certificatePath: "certifications/isro/disaster-risk-reduction.jpg" },
-      { title: "Remote Sensing of Soils: Mapping, Classification and Evaluation for Agricultural Land Use Planning (IIRS) [A+]", year: "2025", certificatePath: "certifications/isro/soil-mapping.jpg" },
-      { title: "AI/ML for Geodata Analysis (IIRS) [A+]", year: "2024", certificatePath: "certifications/isro/aiml-geodata.jpg" },
-      { title: "Automated Feature Extraction from High Resolution Images (IIRS)", year: "2023", certificatePath: "certifications/isro/feature-extraction.jpg" }
+      { title: "Primer on Interferometric SAR Remote Sensing (IIRS-NASA)", year: "2025", certificatePath: "certifications/Course and Softwares/ISRO/Internaational Web Course - ISRO, GoI, NASA - Primer on Interferometric SAR Remote Sensing_page_1.jpg" },
+      { title: "Remote Sensing and Digital Image Analysis (IIRS)", year: "2025", certificatePath: "certifications/Course and Softwares/ISRO/Web Course - ISRO - Remote Sensing and Digital Image Analysis_page_1.jpg" },
+      { title: "Remote Sensing Data Analytics for Crop Production Forecasting (IIRS) [A+]", year: "2025", certificatePath: "certifications/Course and Softwares/ISRO/Web Course - ISRO - Remote Sensing Data Analytics for Crop Production Forecasting_page_1.jpg" },
+      { title: "Geospatial technology for modelling of Urban Environment (IIRS) [A+]", year: "2025", certificatePath: "certifications/Course and Softwares/ISRO/Web Course - ISRO - Geospatial technology for modelling of Urban Environment_page_1.jpg" },
+      { title: "Space Technology for Disaster Risk Reduction (IIRS) [A+]", year: "2025", certificatePath: "certifications/Course and Softwares/ISRO/Web Course - ISRO - Space Technology for Disaster Risk Reduction_page_1.jpg" },
+      { title: "Remote Sensing of Soils: Mapping, Classification and Evaluation for Agricultural Land Use Planning (IIRS) [A+]", year: "2025", certificatePath: "certifications/Course and Softwares/ISRO/Web Course - ISRO - Remote Sensing of Soils Mapping Classification _page_1.jpg" },
+      { title: "AI/ML for Geodata Analysis (IIRS) [A+]", year: "2024", certificatePath: "certifications/Course and Softwares/ISRO/Web Course - ISRO - AL and ML GeoData_page_1.jpg" },
+      { title: "Automated Feature Extraction from High Resolution Images (IIRS)", year: "2023", certificatePath: "certifications/Course and Softwares/ISRO/Web Course - ISRO - Automated Feature Extraction from HR_page_1.jpg" }
     ],
     MOOC: [
-      { title: "The ArcGIS Imagery MOOC: Foundations and Frontiers (ESRI)", year: "2025", certificatePath: "certifications/mooc/arcgis-imagery.jpg" },
-      { title: "Earth Observation for Climate Action (ISRO)", year: "2025", certificatePath: "certifications/mooc/climate-action.jpg" },
-      { title: "Spatial Data Science: The New Frontier in Analytics (ESRI)", year: "2025", certificatePath: "certifications/mooc/spatial-data-science.jpg" },
-      { title: "Cartography (ESRI)", year: "2024", certificatePath: "certifications/mooc/cartography.jpg" },
-      { title: "Going Places with Spatial Analysis (ESRI)", year: "2024", certificatePath: "certifications/mooc/spatial-analysis-places.jpg" }
+      { title: "The ArcGIS Imagery MOOC: Foundations and Frontiers (ESRI)", year: "2025", certificatePath: "certifications/Course and Softwares/MOOC/MOOC - ESRI - The ArcGIS Imagery MOOC Foundations and Frontiers_page_1.jpg" },
+      { title: "Earth Observation for Climate Action (ISRO)", year: "2025", certificatePath: "certifications/Course and Softwares/MOOC/MOOC - ISRO - Earth Observation for Climate Action_page_1.jpg" },
+      { title: "Spatial Data Science: The New Frontier in Analytics (ESRI)", year: "2025", certificatePath: "certifications/Course and Softwares/MOOC/MOOC - ESRI - Spatial Data Science_page_1.jpg" },
+      { title: "Cartography (ESRI)", year: "2024", certificatePath: "certifications/Course and Softwares/MOOC/MOOC - ESRI - Cartography_page_1.jpg" },
+      { title: "Going Places with Spatial Analysis (ESRI)", year: "2024", certificatePath: "certifications/Course and Softwares/MOOC/MOOC - ESRI - Going Places with Spatial Analysis_page_1.jpg" }
     ],
     Udemy: [
-      { title: "Adobe Premiere Pro cc 2020: Video editing for Beginners (#BeAble)", year: "2020", certificatePath: "certifications/udemy/premiere-pro.jpg" },
-      { title: "Adobe Lightroom Masterclass – Beginner to Expert (#BeAble)", year: "2020", certificatePath: "certifications/udemy/lightroom.jpg" }
+      { title: "Adobe Premiere Pro cc 2020: Video editing for Beginners (#BeAble)", year: "2020", certificatePath: "certifications/Course and Softwares/Course -Udemy-Adobe premiere pro_page_1.jpg" },
+      { title: "Adobe Lightroom Masterclass – Beginner to Expert (#BeAble)", year: "2020", certificatePath: "certifications/Course and Softwares/Course - Udemy - Adobe LightRoom_page_1.jpg" }
     ],
     MATLAB: [
-      { title: "MATLAB Onramp (MatWorks)", year: "2025", certificatePath: "certifications/matlab/matlab-onramp.jpg" },
-      { title: "MATLAB Simulink Onramp (MatWorks)", year: "2025", certificatePath: "certifications/matlab/simulink-onramp.jpg" },
-      { title: "Machine Learning Onramp (MatWorks)", year: "2025", certificatePath: "certifications/matlab/ml-onramp.jpg" }
+      { title: "MATLAB Onramp (MatWorks)", year: "2025", certificatePath: "certifications/Course and Softwares/MatLab/1. Matlab Onramp_page_1.jpg" },
+      { title: "MATLAB Simulink Onramp (MatWorks)", year: "2025", certificatePath: "certifications/Course and Softwares/MatLab/2. Simulink Onramp_page_1.jpg" },
+      { title: "Machine Learning Onramp (MatWorks)", year: "2025", certificatePath: "certifications/Course and Softwares/MatLab/3. Machine Learning Onramp_page_1.jpg" }
     ],
     SelfPaced: [
-      { title: "Roadmap to Become a Machine Learning Engineer (Scaler)", year: "2026", certificatePath: "certifications/self-paced/ml-engineer.jpg" },
-      { title: "GenAI Powered Data Analytics Job Simulation (TATA-Forage)", year: "2026", certificatePath: "certifications/self-paced/genai-analytics.jpg" }
+      { title: "Roadmap to Become a Machine Learning Engineer (Scaler)", year: "2026", certificatePath: "certifications/Course and Softwares/Course Certificates/Online course Certificates/Scalar - Roadmap to Machine Learning Engineer.png" },
+      { title: "GenAI Powered Data Analytics Job Simulation (TATA-Forage)", year: "2026", certificatePath: "certifications/Course and Softwares/Course Certificates/Online course Certificates/TATA - GenAI Data Anlaysis_page_1.jpg" }
     ]
   },
   courses: [
-    { title: "Global Warming and Disaster Prevention in Agriculture (0+1) - TNAU (NAHEP)", year: "2023", certificatePath: "certifications/courses/global-warming.jpg" },
-    { title: "MS-Office 2007 (SysSoft)", year: "2014", certificatePath: "certifications/courses/ms-office.jpg" },
-    { title: "C Programme (SysSoft)", year: "2014", certificatePath: "certifications/courses/c-program.jpg" }
+    { title: "Global Warming and Disaster Prevention in Agriculture (0+1) - TNAU (NAHEP)", year: "2023", certificatePath: "certifications/Course and Softwares/Course - NAHEP - Global Warming and Disatser Prevention (0+1)_page_1.jpg" },
+    { title: "MS-Office 2007 (SysSoft)", year: "2014", certificatePath: "certifications/Course and Softwares/Course - syssoft MS Office_page_1.jpg" },
+    { title: "C Programme (SysSoft)", year: "2014", certificatePath: "certifications/Course and Softwares/Course - syssoft C_page_1.jpg" }
   ],
   trainings: [
-    { title: "Bhuvan Overview (ISRO - NRSC)", year: "2025", certificatePath: "certifications/trainings/bhuvan.jpg" },
-    { title: "Data Analysis with MS-Excel (TNAU - PS&IT)", year: "2023", certificatePath: "certifications/trainings/excel-analysis.jpg" }
+    { title: "Bhuvan Overview (ISRO - NRSC)", year: "2025", certificatePath: "certifications/Training/Bhuvan Overview_page_1.jpg" },
+    { title: "Data Analysis with MS-Excel (TNAU - PS&IT)", year: "2023", certificatePath: "certifications/Training/Training - TNAU - Excel_page_1.jpg" }
   ],
   webinars: [
-    { title: "Pharmacovigilance – Department of Industrial Biotechnology and Faculty of Agricultural Science", year: "2020", certificatePath: "certifications/webinars/pharmacovigilance.jpg" }
+    { title: "Pharmacovigilance – Department of Industrial Biotechnology and Faculty of Agricultural Science", year: "2020", certificatePath: "certifications/Webniar/Webinar - Bharath university - Biotech_page_1.jpg" }
   ],
   workshops: [
-    { title: "Empowering Early Career Researchers in Advancing Climate Resilient Agriculture Research and Collaboration (KSNUAHS - NRI, UK & ICSSR)", year: "2026", certificatePath: "certifications/workshops/early-career-researchers.jpg" },
-    { title: "Meeting the Essentials of Academic Deliberation in Graduate Seminar and Thesis Examination (TNAU - DNRM)", year: "2023", certificatePath: "certifications/workshops/academic-deliberation.jpg" },
-    { title: "Edible Mushroom Cultivation for Self-Employment (SRSIAT)", year: "2019", certificatePath: "certifications/workshops/mushroom-cultivation.jpg" }
+    { title: "Empowering Early Career Researchers in Advancing Climate Resilient Agriculture Research and Collaboration (KSNUAHS - NRI, UK & ICSSR)", year: "2026", certificatePath: "certifications/Workshop/Workshop - CRARC Shivamogaa 2026_page_1.jpg" },
+    { title: "Meeting the Essentials of Academic Deliberation in Graduate Seminar and Thesis Examination (TNAU - DNRM)", year: "2023", certificatePath: "certifications/Workshop/Workshop - NRM ENS_page_1.jpg" },
+    { title: "Edible Mushroom Cultivation for Self-Employment (SRSIAT)", year: "2019", certificatePath: "certifications/Workshop/Workshop - mushroom_page_1.jpg" }
   ],
   conferences: [
     { title: "Sustainable Agriculture and Farmers Well-being: Digitalize, Diversify, Decarbonise and Develop (TNAU - 10th AGSC) [Poster]", year: "2025", certificatePath: "certifications/conferences/sustainable-agriculture.jpg" },
     { title: "9th AGSC (TNAU)", year: "2024", certificatePath: "certifications/conferences/agsc-9.jpg" },
-    { title: "Globalization of Agriculture: Food, Feed & Fuel (TNAU - 8th AGSC) [Participated]", year: "2023", certificatePath: "certifications/conferences/globalization-agriculture.jpg" }
+    { title: "Globalization of Agriculture: Food, Feed & Fuel (TNAU - 8th AGSC) [Participated]", year: "2023", certificatePath: "certifications/Conference/Conference AGSC - 8th AGSC_page_1.jpg" }
   ],
   invitedTalks: [
     { title: "Soil Survey and Digital Mapping using Remote Sensing - Practical hands-on Digital Mapping of the Western Ghats Forest Soil Resources using QGIS (GPSIAM)", year: "2025", certificatePath: "certifications/invited-talks/soil-survey-qgis.jpg" }
